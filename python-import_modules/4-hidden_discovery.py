f54290950bdf36a5d9f6d193c9cddc9b1d99cd1e
GhaidaAl36 <ghaida3638@gmail.com>
2025-11-28 11:51:02 +0000

    Add hidden discovery script and .pyc file

---
+++ python-import_modules/4-hidden_discovery.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+import marshal
+
+if __name__ == "__main__":
+    with open("/tmp/hidden_4.pyc", "rb") as f:
+        f.read(16)  # skip header
+        code = marshal.load(f)
+
+    names = [name for name in code.co_names if not name.startswith("__")]
+    for name in sorted(names):
+        print(name)
