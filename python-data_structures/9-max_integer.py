74c1265fa469052a35bb167a63e9f398da623ebe
GhaidaAl36 <ghaida3638@gmail.com>
2025-12-01 10:34:17 +0000

    handel -

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    maximu = 0
     if not my_list:
         return (None)
+    maximu = my_list[0]
     for x in my_list:
         if x > maximu:
             maximu = x
