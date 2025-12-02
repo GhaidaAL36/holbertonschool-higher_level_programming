cf1836fedc72b526a20fba45cd8a17d4546e1fdf
GhaidaAl36 <ghaida3638@gmail.com>
2025-12-02 09:43:22 +0000

    Pycodestyle validation

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
     if not isinstance(roman_string, str) or roman_string is None:
-        return(0)
+        return (0)
     roman_dict = {'I': 1, 'V': 5, 'X': 10, 'L': 50,
                   'C': 100, 'D': 500, 'M': 1000}
     total = 0
@@ -12,5 +12,5 @@ def roman_to_int(roman_string):
             total -= value
         else:
             total += value
-        prev_value = value
-    return(total)
+            prev_value = value
+    return (total)
