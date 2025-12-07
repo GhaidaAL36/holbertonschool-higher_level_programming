45c6f7b3cd8b98ad1a7c8a3264507f1c4107c729
GhaidaAl36 <ghaida3638@gmail.com>
2025-12-07 10:07:49 +0300

    adding empty line at the top and at the end

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-"""This module defines an empty class Square."""
+"""This module defines an empty class Square used to represent a square object without any attributes or methods."""
+
+
 class Square:
     """Represents an empty square."""
     pass
