4c785822fbec2b44693982ca01077ec059b40204
GhaidaAl36 <ghaida3638@gmail.com>
2025-12-09 09:31:45 +0300

    edit doc

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -3,16 +3,16 @@
 
 
 class Square:
-    """Represent a square with private optional attribute """
+    """Represent a square with private optional attribute."""
+
     def __init__(self, size=0):
-        if not isinstance(size, int): 
-            """
-            Initialize a Square instance.
-            
-            The size of the square (must be an integer >= 0)
-            """
+        """
+        Initialize a Square instance.
+
+        :param size: The size of the square (must be an integer >= 0)
+        """
+        if not isinstance(size, int):
             raise TypeError("size must be an integer")
         if size < 0:
             raise ValueError("size must be >= 0")
-        
         self.__size = size
