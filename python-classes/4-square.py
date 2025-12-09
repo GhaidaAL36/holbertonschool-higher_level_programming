676fbbf81d997694043f6bf9d01bd4a3a5620b97
GhaidaAl36 <ghaida3638@gmail.com>
2025-12-09 19:25:42 +0300

    edit the doc

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -3,26 +3,27 @@
 
 
 class Square:
-    """Represent a square with private optional attribute."""
+    """This class defines a square with a specififc size"""
     def __init__(self, size=0):
         self.size = size
-    
+
     @property
     def size(self):
         return self.__size
         """returns the size of the square"""
-    
+
     @size.setter
     def size(self, value):
         if not isinstance(value, int):
             raise TypeError("size must be an integer")
+        """TypeError: if size is not an integer"""
         if value < 0:
             raise ValueError("size must be >= 0")
+        """ValueError: if size is less than 0"""
         self.__size = value
         """Returns the value"""
- 
+
     def area(self):
-        """
-        return the square area
-        """
-        return self.size * self.size
+        """Calculate the area of the square"""
+        return self.__size ** 2
+        """returns the area of the square"""
