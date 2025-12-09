310439ce71ec6b3bbb4c4909ebb65797878e5ca3
GhaidaAl36 <ghaida3638@gmail.com>
2025-12-09 10:04:43 +0300

    task 3

---
+++ python-classes/3-square.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""This class defines a square"""
+
+
+class Square:
+    """Represent a square with private optional attribute."""
+
+    def __init__(self, size=0):
+        """
+        Initialize a Square instance.
+
+        :param size: The size of the square (must be an integer >= 0)
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """
+        return the square area
+        """
+        return self.__size * self.__size
