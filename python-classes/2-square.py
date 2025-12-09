f51fbcea30b0dc0cdaba67c352831cca7627a3c6
GhaidaAl36 <ghaida3638@gmail.com>
2025-12-09 09:33:22 +0300

    last try with the doc

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -1,18 +1,25 @@
 #!/usr/bin/python3
-"""defines a square with size validation"""
+"""This class defines a square"""
 
 
 class Square:
-    """Represent a square with private optional attribute."""
-
+    """This class defines a square with a specific size"""
     def __init__(self, size=0):
         """
-        Initialize a Square instance.
-
-        :param size: The size of the square (must be an integer >= 0)
+        Initialize a new square instance.
+        size: the size of the square
         """
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
+        """TypeError: if size is not an integer
+        """
+
         if size < 0:
             raise ValueError("size must be >= 0")
+        """"
+        ValueError: if size is less than 0
+        """
+
         self.__size = size
+        """the size of the square, must not be negative
+        """
