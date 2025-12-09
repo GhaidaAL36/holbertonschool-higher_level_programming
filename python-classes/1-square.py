dc388827c36a3fad6af4a185ade98ee7b4b3df4e
GhaidaAl36 <ghaida3638@gmail.com>
2025-12-09 08:51:47 +0300

    task 1

---
+++ python-classes/1-square.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""defines a square by Private instance attribute: size"""
+
+
+class Square:
+    """Represent a square with private attribute"""
+    def __init__(self, size):
+        self.__size = size
