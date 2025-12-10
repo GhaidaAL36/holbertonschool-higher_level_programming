694506a7f713c398a8336ee2fac02807d8746767
GhaidaAl36 <ghaida3638@gmail.com>
2025-12-10 11:36:20 +0300

    Pycodestyle validation - line too long

--- python-more_classes/7-rectangle.py
+++ python-more_classes/7-rectangle.py
@@ -60,7 +60,9 @@ class Rectangle:
         """print the rectangle with the character #"""
         if self.__width == 0 or self.__height == 0:
             return ""
-        return "\n".join(str(self.print_symbol) * self.width for _ in range(self.height))
+        return "\n".join(
+        str(self.print_symbol) * self.width for _ in range(self.height)
+    )
 
     def __repr__(self):
         """return a string representation of the rectangle"""
