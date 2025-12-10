51fa16fa589bc5c571048b7a0b9783be817d9d73
GhaidaAl36 <ghaida3638@gmail.com>
2025-12-10 11:39:00 +0300

    task 7

--- python-more_classes/7-rectangle.py
+++ python-more_classes/7-rectangle.py
@@ -63,7 +63,7 @@ class Rectangle:
         return "\n".join(
         str(self.print_symbol) * self.width for _ in range(self.height)
     )
-
+        
     def __repr__(self):
         """return a string representation of the rectangle"""
         return f"Rectangle({self.__width}, {self.__height})"
