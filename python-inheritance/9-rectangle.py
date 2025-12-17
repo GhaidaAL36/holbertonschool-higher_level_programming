8aaa5b2b008a40408963924e6e33be1bb4eb77e6
GhaidaAl36 <ghaida3638@gmail.com>
2025-12-17 13:29:13 +0300

    PEP8 validation - remove extra space

--- python-inheritance/9-rectangle.py
+++ python-inheritance/9-rectangle.py
@@ -16,7 +16,7 @@ class Rectangle(BaseGeometry):
 
         self.__width = width
         self.__height = height
-    
+
     def area(self):
         """return area of rectangle"""
         return self.__width * self.__height
