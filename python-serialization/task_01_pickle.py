475ce2ba3f7f73caca2845d03c613a03238991a9
GhaidaAl36 <ghaida3638@gmail.com>
2025-12-23 11:16:38 +0300

    handle format

--- python-serialization/task_01_pickle.py
+++ python-serialization/task_01_pickle.py
@@ -1,15 +1,19 @@
 import pickle
+
+
 class CustomObject:
     def __init__(self, name, age, is_student):
         self.name = name
         self.age = age
         self.is_student = is_student
+
     def display(self):
         print(f"Name: {self.name}\nAge: {self.age}\nIs_student: {self.is_student}")
-    
+
     def serialize(self, filename):
         with open(filename, "wb") as file:
             pickle.dump(self, file)
+
     @classmethod
     def deserialize(cls, filename):
         with open(filename, "rb") as file:
