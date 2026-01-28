e3dd49d8ad6b6217edabf624cbda2e38ff39441a
GhaidaAl36 <ghaida3638@gmail.com>
2026-01-28 19:06:34 +0300

    remove space

--- javascript-warm_up/6-multi_languages_loop.js
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -1,11 +1,11 @@
 #!/usr/bin/node
 
 const lines = [
-    'C is fun',
-    'Python is cool',
-    'JavaScript is amazing'
+  'C is fun',
+  'Python is cool',
+  'JavaScript is amazing'
 ];
 
 for (const line of lines) {
-    console.log(line);
+  console.log(line);
 }
