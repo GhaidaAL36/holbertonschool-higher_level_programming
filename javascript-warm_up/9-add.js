cc81a99f70400324f7378987cf657b1f4e76456c
GhaidaAl36 <ghaida3638@gmail.com>
2026-01-28 19:34:29 +0300

    add space

--- javascript-warm_up/9-add.js
+++ javascript-warm_up/9-add.js
@@ -3,15 +3,15 @@
 const arg1 = process.argv[2];
 const arg2 = process.argv[3];
 
-function add(a, b) {
-    a = Number(arg1);
-    b = Number(arg2);
+function add (a, b) {
+  a = Number(arg1);
+  b = Number(arg2);
 
-    if (a && b) {
-      return (a + b);
-    } else {
-      return (NaN);
-    }
+  if (a && b) {
+    return (a + b);
+  } else {
+    return (NaN);
+  }
 }
 
 console.log(add(arg1, arg2));
