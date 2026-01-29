0ab3f47509a89f12290043144b180551fb7b413a
GhaidaAl36 <ghaida3638@gmail.com>
2026-01-29 12:09:09 +0300

    rename consts + remove space

--- javascript-warm_up/11-second_biggest.js
+++ javascript-warm_up/11-second_biggest.js
@@ -1,15 +1,16 @@
 #!/usr/bin/node
 
-const numbers_list = process.argv.slice(2).map(Number);
-const max_num = Math.max(...numbers_list);
-let second_max = Math.min(...numbers_list);
+const numbersList = process.argv.slice(2).map(Number);
+const maxNum = Math.max(...numbersList);
+let secondMax = Math.min(...numbersList);
 
-if (numbers_list.length === 0 || numbers_list.length === 1) {
-    console.log(0)
+if (numbersList.length === 0 || numbersList.length === 1) {
+  console.log(0);
 } else {
-    for (let i = 0; i < numbers_list.length; i++) {
-        if (second_max < numbers_list[i] && numbers_list[i] < max_num) {
-            second_max = numbers_list[i];
-        }
-    } console.log(second_max);
+  for (let i = 0; i < numbersList.length; i++) {
+    if (secondMax < numbersList[i] && numbersList[i] < maxNum) {
+      secondMax = numbersList[i];
+    }
+  }
+  console.log(secondMax);
 }
